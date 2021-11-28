8e72db8c102d3beee40484cd3324a637b70940ac
John.Hamer <57878287+JohnMODLR@users.noreply.github.com>
2021-11-29 12:59:21 +1300

    Update Utility.Copy Scenario.Extended.js

--- Utilities/Utility.Copy Scenario.Extended.js
+++ Utilities/Utility.Copy Scenario.Extended.js
@@ -29,13 +29,13 @@ function begin() {
     for(var i=0;i<dims.length;i++) {
         var dim = dims[i];
         if( dimension.getType(dim.id) == "scenario" ) { 
-            scenarioPosititon = i;
+            scenarioPosition = i;
             break;
         }
     }
 
     //abort if we can not find the scenario dimension.
-    if( scenarioPosititon == -1 ) {
+    if( scenarioPosition == -1 ) {
         script.abort("Could not find the Scenario dimension within the target cube.");
         return;
     }
@@ -58,14 +58,14 @@ function begin() {
     }
 
     //abort if the from scenario doesnt exist
-    var scenarioDim = dims[scenarioPosititon];
+    var scenarioDim = dims[scenarioPosition];
     if( element.exists(scenarioDim.name,scenario_from) == false ) {
         script.abort("Could not find the Scenario: "+scenario_from+" within the scenario dimension: " + scenarioDim.name + ".");
         return;
     }
 
     //abort if the to scenario doesnt exist
-    var scenarioDim = dims[scenarioPosititon];
+    var scenarioDim = dims[scenarioPosition];
     if( element.exists(scenarioDim.name,scenario_to) == false ) {
         script.abort("Could not find the Scenario: "+scenario_to+" within the scenario dimension: " + scenarioDim.name + ".");
         return;
@@ -75,7 +75,7 @@ function begin() {
     if( should_wipe == "1" ) {
         var elements = [];
         for(var i=0;i<dims.length;i++) {
-            if( i == scenarioPosititon ) {
+            if( i == scenarioPosition ) {
                 elements.push(scenario_to);    //add the destination scenario for clearing
             } else if( i == dimFilterPosition ) {
                 elements.push(element_filter);    //add the destination dimension filter for clearing
@@ -89,7 +89,7 @@ function begin() {
     }
 
     //change the element list to from slice options
-    elements[scenarioPosititon] = scenario_from;
+    elements[scenarioPosition] = scenario_from;
 
     //turn cube logging off (as it is unnecessary)
     cube.log(cube_name,false);
@@ -103,7 +103,7 @@ function begin() {
         //build a new target location (array of element names)
         var elmsSet = [];
         for(var i=0;i<elms.length-1;i++){
-            if( i == scenarioPosititon ) {    //ensure that we are "looking at" the destination scenario
+            if( i == scenarioPosition ) {    //ensure that we are "looking at" the destination scenario
                 elmsSet.push(scenario_to);
             } else if( i == dimFilterPosition ) {
                 elmsSet.push(element_filter);    //add the dimension filter with element
